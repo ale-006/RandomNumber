84089ef28283a424fc314438e9f7b979e6a52e4a
Alessandro Baio <77743955+ale-006@users.noreply.github.com>
2021-10-29 15:33:14 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function example() {
+    let numPiccolo = Math.floor(Math.random() * 30);
+    let numGrande = Math.floor(Math.random() * (60 - 30)) + 30
+    document.getElementById("small").value = numPiccolo;
+    document.getElementById("high").value = numGrande;
+}
+function random() {
+    let min = document.getElementById("small").value
+    let max = document.getElementById("high").value
+    let result = Math.floor(Math.random() * (max - min)) + parseInt(min)
+    let output = "";
+    if(isNaN(result)) {
+        document.getElementById("result").innerHTML = "Error!"
+    }
+    else {
+    document.getElementById("result").innerHTML = result
+    }
+}

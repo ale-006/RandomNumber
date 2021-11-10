cee8b6bd440bc07c5c907e045bc2b122d639252e
Alessandro Baio <77743955+ale-006@users.noreply.github.com>
2021-11-10 09:01:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,11 +8,10 @@ function random() {
     let min = document.getElementById("small").value
     let max = document.getElementById("high").value
     let result = Math.floor(Math.random() * (max - min + 1)) + parseInt(min)
-    let output = "";
     if(isNaN(result)) {
         document.getElementById("result").innerHTML = "Error!"
     }
     else {
-    document.getElementById("result").innerHTML = "10"
+    document.getElementById("result").innerHTML = result
     }
 }
